b8751c7261ffcf3bfe4049cc2ca3ddd2d22b8056
root <chagmgang@si-analytics.ai>
2021-03-02 11:16:36 +0000

    a

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ def run_epoch(net, criterion, optimizer, dataloader, device, is_train):
     loss_list = []
 
     if is_train:
-        model = model.train()
+        net = net.train()
     else:
-        model = model.eval()
+        net = net.train()
 
     for data in tqdm.tqdm(dataloader):
 
@@ -35,24 +35,28 @@ def run_epoch(net, criterion, optimizer, dataloader, device, is_train):
         pred = torch.argmax(logit, axis=1)
         pred = pred.eq(target)
 
-        loss_list.append(loss.item())
-        pred_list.extend(pred.item())
+        loss = loss.detach().cpu().numpy()
+        pred = pred.detach().cpu().numpy()
 
-    loss_mean = torch.mean(torch.stack(loss_list))
-    pred_mean = torch.mean(torch.stack(pred_list))
+        loss_list.append(loss)
+        pred_list.extend(pred)
+
+    loss_mean = sum(loss_list) / len(loss_list)
+    pred_mean = sum(pred_list) / len(pred_list)
 
     return loss_mean, pred_mean
 
 def main():
 
     device = torch.device(0) if torch.cuda.is_available() else torch.device('cpu')
-    train_dataloader = datasets.make_dataset(is_train=True, batch_size=32)
-    valid_dataloader = datasets.make_dataset(is_train=False, batch_size=32)
+    train_dataloader = datasets.make_dataset(is_train=True, batch_size=128)
+    valid_dataloader = datasets.make_dataset(is_train=False, batch_size=128)
 
     net = model.Model(num_class=100)
+    net = net.to(device)
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=0.0001, momentum=0.9)
-    epochs = 30
+    epochs = 100
 
     for epoch in range(epochs):
 
